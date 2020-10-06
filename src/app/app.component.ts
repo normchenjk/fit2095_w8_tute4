8c0ccadafe04826962998462d11140d9f078b0e3
Norman Chen <normchenjk@users.noreply.github.com>
2020-10-06 16:09:39 +1100

    Added code for the Angular code covered during the tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'w8tute4';
+
+  imageUrl = "https://images.unsplash.com/photo-1587293852726-70cdb56c2866?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80";
+
+  items = [];
+
+  itemName: string = "";
+  itemQuantity: number = 0;
+  itemPrice: number = 0;
+
+  saveItem() {
+    console.log("Form submitted!");
+    console.log(this.itemName, this.itemQuantity, this.itemPrice);
+
+    this.items.push({
+      name: this.itemName,
+      quantity: this.itemQuantity,
+      price: this.itemPrice
+    })
+  }
 }
